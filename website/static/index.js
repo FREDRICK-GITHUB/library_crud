03ba2bd09d3250c7705776efa6fa3d5910e1ba5e
FREDRICK-GITHUB <kamaufredm@gmail.com>
2024-02-27 11:34:58 +0300

    added transaction view files

--- website/static/index.js
+++ website/static/index.js
@@ -1,8 +1,6 @@
-// function deleteNote(noteId) {
-//     fetch("/delete-note", {
-//         method: "POST",
-//         body: JSON.stringify({ noteId: noteId }),
-//     }).then((_res) => {
-//         window.location.href = "/";
-//     });
-// }
+
+$(function () {
+    $("#date").datepicker({
+        dateFormat: "dd-mm-yy" 
+    });
+});
